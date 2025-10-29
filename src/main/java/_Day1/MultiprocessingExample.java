4fb40b114fdbf2836959f72f7a3d36d723364162
Michael Schimpf <mschimpf@scu.edu>
2025-10-29 12:16:57 -0400

    Added processors

--- src/main/java/_Day1/MultiprocessingExample.java
+++ src/main/java/_Day1/MultiprocessingExample.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MultiprocessingExample {
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Available processors: "+Runtime.getRuntime().availableProcessors());
     	ProcessBuilder processBuilder = new ProcessBuilder(
                 "java",
                 "-cp",
